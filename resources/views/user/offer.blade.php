d59e5735ba708f1c3bcfe13f3b9a259b1fd0f889
matsuaya <aya.k.m326@gmail.com>
2020-12-30 09:37:19 +0000

    申請状況確認用テーブルを画面に追加

--- resources/views/user/offer.blade.php
+++ resources/views/user/offer.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{ secure_asset('css/form.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/calendar.css') }}" rel="stylesheet">
         <title>申請画面</title>
     </head>
     <body>
@@ -36,8 +37,39 @@
             <input type="hidden" name="application_status" value="0">
             {{ csrf_field() }}
             <input type="submit" class="btn_apply" value="申請">
-        </div>
         </fieldset>
         </form>
+        
+        <CENTER>
+        <h2>申請状況</h2>
+        <table class="table table-bordered" bordere=5>
+        <thead class="thead-dark">
+            <tr>
+                <th width="15%">日付</th>
+                <th width="15%">社員ID</th>
+                <th width="20%">名前</th>
+                <th width="20%">予定取得時間</th>
+                <th width="15%">申請状況</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($applications as $rest)
+            <tr>
+                <td>{{ $rest->day->format( 'Y-m-d' ) }}</td>
+                <td>{{ $rest->employee_code }}</td>
+                <td>{{ $rest->name }}</td>
+                <td>{{ $rest->plan_rest_time }}</td>
+                    @if($rest->application_status==1)
+                        <td>承認</td>
+                    @elseif($rest->application_status==2)
+                        <td>否認</td>
+                    @else
+                        <td>未確認</td>
+                    @endif
+            </tr>
+            @endforeach
+        </tbody>
+        </table>
+        </CENTER>
     </body>
 </html>
